1be5d30b9ddb0a7e46e63d1b0d1473c0a5d9f2bb
Sudhanshu <sudhanshu007@bhu.ac.in>
2025-10-30 13:28:41 +0530

    add insert at begin

---
+++ Date wise/30-10-25/insertAtBeginLL.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int val;
+	Node* next;
+};
+
+Node* create(int a){
+	Node* n = new Node();
+	n->val = a;
+	n->next = NULL;
+	return n;
+}
+
+Node* insertAtBegin(Node* head, int x){
+	Node* n = create(x);
+	n->next = head;
+	head = n;
+	return head;
+
+}
+
+void print(Node* head){
+	while(head != NULL){
+		cout<<head->val<<" ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+int main(){
+	Node* head = create(10);
+	Node* a = create(20);
+	Node* b = create(30);
+	Node* c = create(40);
+
+	head->next = a;
+	a->next = b;
+	b->next = c;
+
+	cout<<"before Insertion: ";
+	print(head);
+
+	head = insertAtBegin(head, 50);
+
+	cout<<"after insertion: ";
+	print(head);
+
+	cout<<"third insertion: ";
+	head = insertAtBegin(head, 60);
+	print(head);
+
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
+
